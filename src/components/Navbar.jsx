c204226b89e1bf0fa763b9fbd46dfce0f25bfe43
Braison Wabwire <braisonwabwire2003@gmail.com>
2025-05-03 15:36:34 +0300

    Creating Nav bar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function Navbar() {
+
+    const [menuOpen, setMenuOpen] = React.useState(false);
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">MyPortfolio</div>
+
+      <div className={`navbar-links ${menuOpen ? 'active' : ''}`}>
+        <a href="#hero">Home</a>
+        <a href="#about">About</a>
+        <a href="#projects">Projects</a>
+        <a href="#skills">Skills</a>
+        <a href="#contact">Contact</a>
+      </div>
+
+      <div
+        className={`menu-toggle ${menuOpen ? 'open' : ''}`}
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar

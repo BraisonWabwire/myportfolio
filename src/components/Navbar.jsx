72f811fa506cad80c6a951246d02be8806b6d430
Braison Wabwire <braisonwabwire2003@gmail.com>
2025-05-03 18:17:00 +0300

    Updating the navbar to automatically close when using a phone

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,37 @@
-import React from 'react'
-import './Navbar.css'; // Import the CSS file for styling
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
-
+import React from 'react';
+import './Navbar.css';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = React.useState(false);
 
-    const [menuOpen, setMenuOpen] = React.useState(false);
+  // Function to close menu on link click
+  const handleLinkClick = () => {
+    if (menuOpen) {
+      setMenuOpen(false);
+    }
+  };
 
   return (
     <nav className="navbar">
       <div className="navbar-logo">MyPortfolio</div>
 
       <div className={`navbar-links ${menuOpen ? 'active' : ''}`}>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/projects">Projects</Link>
-        <Link to="/skills">Skills</Link>
-        <Link to="/contact">Contact</Link>
+        <Link to="/" onClick={handleLinkClick}>
+          Home
+        </Link>
+        <Link to="/about" onClick={handleLinkClick}>
+          About
+        </Link>
+        <Link to="/projects" onClick={handleLinkClick}>
+          Projects
+        </Link>
+        <Link to="/skills" onClick={handleLinkClick}>
+          Skills
+        </Link>
+        <Link to="/contact" onClick={handleLinkClick}>
+          Contact
+        </Link>
       </div>
 
       <div
@@ -28,7 +43,7 @@ function Navbar() {
         <div className="bar"></div>
       </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
